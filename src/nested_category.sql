a8d980eb921236bd1ef4940a625bdaefc09883f0
Dinesh V <dineshshabde@gmail.com>
2015-11-28 16:52:38 +0530

    Added database file

---
+++ src/nested_category.sql
@@ -0,0 +1,14 @@
+TRUNCATE nested_category;
+
+INSERT INTO nested_category
+VALUES
+	(1, 'ELECTRONICS', 1, 20, 0),
+	(2, 'TELEVISIONS', 2, 9, 1),
+	(3, 'TUBE', 3, 4, 2),
+	(4, 'LCD', 5, 6, 2),
+	(5, 'PLASMA', 7, 8, 2),
+	(6,'PORTABLE ELECTRONICS',10,19,1),
+	(7, 'MP3 PLAYERS', 11, 14, 6),
+	(8, 'FLASH', 12, 13, 7),
+	(9, 'CD PLAYERS', 15, 16, 6),
+	(10, '2 WAY RADIOS', 17, 18, 6);
